19c5802294a66a7ee12f3acd0fe037cefe526758
John Internet <jonjon1234.github@gmail.com>
2019-09-09 19:39:07 +0100

    Add more tests and unify the validator in the setup function.

--- tests/ListValidatorTest.php
+++ tests/ListValidatorTest.php
@@ -5,35 +5,58 @@ use PHPUnit\Framework\TestCase;
 use Internet\InterValid\ListValidator;
 
 final class ListValidatorTest extends TestCase {
-	public $testList = [
-		"MY_VAR" => "hello, world",
-		"MY_INT" => 12,
-		"MY_FLOAT" => 15.7,
-		"MY_BOOL" => false,
-		"MY_BOOL_STR" => "yes"
-	];
+
+	public $data;
+	/** @var ListValidator */
+	public $validator;
+	protected function setUp(): void{
+		$this->list = [
+			"MY_VAR" => "hello, world",
+			"MY_INT" => 12,
+			"MY_FLOAT" => 15.7,
+			"MY_BOOL" => false,
+			"MY_BOOL_STR" => "yes",
+			"MY_LIST" => "a,b,c,d"
+		];
+
+		$this->validator = new ListValidator($list);
+	}
 
 	public function testCanBeCreated(): void{
 		$this->expectNotToPerformAssertions();
-		$validator = new ListValidator($this->testList);
+		$validator = new ListValidator();
 	}
 
 	public function testHasReturnsTrueIfExists(): void{
-		$validator = new ListValidator($this->testList);
-		$this->assertTrue($validator->has("MY_VAR"));
+		$this->assertTrue($this->validator->has("MY_VAR"));
 	}
 
 	public function testHasReturnsFalseIfNotExists(): void{
-		$validator = new ListValidator($this->testList);
-		$this->assertFalse($validator->has("BAD_KEY"));
+		$this->assertFalse($this->validator->has("BAD_KEY"));
 	}
 
 	public function testIntReturnsIntegers(): void{
-		$validator = new ListValidator($this->testList);
-		$this->assertEquals(12, $validator->int("MY_INT"));
-		$this->assertFalse($validator->int("MY_FLOAT"));
+		$this->assertEquals(12, $this->validator->int("MY_INT"));
+		$this->assertFalse($this->validator->int("MY_FLOAT"));
 
-		$this->assertEquals(20, $validator->int("MY_FLOAT", 20));
-		$this->assertFalse($validator->int("MY_INT", 0, 1, 10));
+		$this->assertEquals(20, $this->validator->int("MY_FLOAT", 20));
+		$this->assertFalse($this->validator->int("MY_INT", 0, 1, 10));
+	}
+
+	public function testBoolReturnsBooleans(): void{
+		$this->assertFalse($this->validator->bool("MY_BOOL"));
+		$this->assertTrue($this->validator->bool("MY_BOOL_STR"));
+	}
+
+	public function testBoolCSVList(): void{
+		$this->assertEquals(['a', 'b', 'c', 'd'], $this->validator->commaList("MY_LIST"));
+	}
+
+	public function testRaw(): void{
+		$this->assertEquals("a,b,c,d", $this->validator->raw("MY_LIST"));
+	}
+
+	public function testData(): void{
+		$this->assertEquals($this->data, $this->validator->data());
 	}
 }
