cacd590b50593e1efa98a82a9b03061e0f4b4335
John Internet <jonjon1234.github@gmail.com>
2019-09-09 14:46:17 +0100

    Add inheritdoc for src.

--- src/ListValidator.php
+++ src/ListValidator.php
@@ -13,10 +13,12 @@ class ListValidator implements Validator {
 		$this->data = $data;
 	}
 
+	/** {@inheritDoc} */
 	public function has($key){
 		return isset($this->data[$key]);
 	}
 
+	/** {@inheritDoc} */
 	public function int($name, $def = false, $min = false, $max = false){
 		$opts = [];
 		if ($def){
@@ -32,22 +34,27 @@ class ListValidator implements Validator {
 		return filter_var($this->raw($name), FILTER_VALIDATE_INT, ['options' => $opts]);
 	}
 
+	/** {@inheritDoc} */
 	public function bool($name){
 		return filter_var($this->raw($name), FILTER_VALIDATE_BOOLEAN);
 	}
 
+	/** {@inheritDoc} */
 	public function bulk($filter, $add = false){
 		return filter_var_array($this->data, $filter, $add);
 	}
 
+	/** {@inheritDoc} */
 	public function commaList($name){
 		return array_filter(array_map("trim", explode(',', $this->raw($name) ?: '')));
 	}
 
+	/** {@inheritDoc} */
 	public function raw($name){
 		return @filter_var($this->data[$name], FILTER_UNSAFE_RAW);
 	}
 
+	/** {@inheritDoc} */
 	public function data(){
 		return filter_var_array($this->data, FILTER_UNSAFE_RAW);
 	}
